75be52f1ca08d3149f96650e5bb7bb2a22877eec
Todd Kushner <kushnertodd@gmail.com>
2025-10-08 11:58:50 -0600

    button decrement works; quick button press on minimal size sometimes grows, missing button press

---
+++ app/src/main/java/com/toddkushnerllc/android_adaptive_ui/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.toddkushnerllc.android_adaptive_ui
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.toddkushnerllc.android_adaptive_ui.ui.theme.AndroidadaptiveuiTheme
+
+private const val MAIN_TAG = "MainActivity"
+
+// this is a change
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AndroidadaptiveuiTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    //DrawingApp()
+                    LogPointerEvents(/*150.dp, 100.dp, 16.dp, null*/)
+                }
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(MAIN_TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(MAIN_TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(MAIN_TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(MAIN_TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(MAIN_TAG, "onDestroy")
+    }
+
+}
---
+++ app/src/main/java/com/toddkushnerllc/android_adaptive_ui/PointerEvents.kt
@@ -0,0 +1,169 @@
+package com.toddkushnerllc.android_adaptive_ui
+
+import android.util.Log
+import androidx.compose.ui.input.pointer.PointerEvent
+import androidx.compose.ui.input.pointer.PointerEventType
+
+object PointerEvents {
+    fun log(message: String) = Log.d("LogPointerEvents", message)
+    val onBoxPointerEvent: (
+        PointerEvent,
+        State,
+        stateChanged: (State) -> Unit
+    ) -> Unit =
+        { event,
+          state,
+          stateChanged
+            ->
+            log("box    ${event.type}, ${state.pointerEventState}, ${event.changes.first().position}, ${event.changes.first().pressure}, ${event.changes.first().uptimeMillis}")
+            when (event.type) {
+                PointerEventType.Press -> {
+                    if (state.buttonMoving) {
+                        state.setFirstPosition(event.changes.first().position)
+                    }
+                    when (state.pointerEventState) {
+                        PointerEventState.START -> {
+                            state.setPointerEventState(PointerEventState.BOX_PRESS)
+                        }
+
+                        PointerEventState.BOX_PRESS -> {}
+
+                        PointerEventState.BUTTON_PRESS -> {
+                            state.setPointerEventState(PointerEventState.BUTTON_BOX_PRESS)
+                        }
+
+                        else -> {
+                            log("unexpected box event type ${event.type} in state ${state.pointerEventState}")
+                            state.setPointerEventState(PointerEventState.START)
+                        }
+                    }
+                }
+
+                PointerEventType.Move -> {
+                    if (state.buttonMoving) {
+                        state.setChangePosition(event.changes.first())
+                    }
+                    when (state.pointerEventState) {
+                        PointerEventState.BOX_PRESS -> {
+                            state.setPointerEventState(PointerEventState.BOX_MOVE)
+                        }
+
+                        PointerEventState.BOX_MOVE -> {
+                        }
+
+                        PointerEventState.BUTTON_MOVE -> {
+                        }
+
+                        else -> {
+                            log("unexpected box event type ${event.type} in state ${state.pointerEventState}")
+                            state.setPointerEventState(PointerEventState.START)
+                        }
+                    }
+                }
+
+                PointerEventType.Release -> {
+                    if (state.buttonMoving) {
+                        state.setFinalPosition()
+                        state.setButtonMoving(false)
+                    }
+                    when (state.pointerEventState) {
+                        PointerEventState.BOX_PRESS -> {
+                            state.setPointerEventState(PointerEventState.START)
+                            state.incrementButtonSize()
+                        }
+
+                        PointerEventState.BOX_MOVE -> {
+                            state.setPointerEventState(PointerEventState.START)
+                            state.incrementButtonSize()
+                        }
+
+                        PointerEventState.BUTTON_RELEASE -> {
+                            state.setPointerEventState(PointerEventState.START)
+                            //if (state.setButtonRelease(event.changes.first().uptimeMillis)) {
+                            //if (state.buttonSizeIndex > (ButtonParameters.buttonSizeIndexMax / 2)) {
+                            //    state.setShowDialog()
+                            //} else {
+                            state.decrementButtonSize()
+                            //}
+                            //}
+                        }
+
+                        else -> {
+                            log("unexpected box event type ${event.type} in state ${state.pointerEventState}")
+                            state.setPointerEventState(PointerEventState.START)
+                        }
+                    }
+                }
+
+                PointerEventType.Move -> {}
+                else ->
+                    log("unexpected box event type ${event.type}")
+            }
+            if (state.dirty)
+                stateChanged(state)
+        }
+    val onButtonPointerEvent: (
+        Int,
+        PointerEvent,
+        State,
+        stateChanged: (State) -> Unit
+    ) -> Unit =
+        { buttonNumber, event, state, stateChanged
+            ->
+            log("button ${buttonNumber}, ${event.type}, ${state.pointerEventState}, ${event.changes.first().position}, pressure ${event.changes.first().pressure}, uptime ${event.changes.first().uptimeMillis}                               ")
+            when (event.type) {
+                PointerEventType.Press -> {
+                    state.setButtonMoving(true)
+                    when (state.pointerEventState) {
+                        PointerEventState.START -> {
+                            state.setPointerEventState(PointerEventState.BUTTON_PRESS)
+                            state.setButtonPress(event.changes.first().uptimeMillis)
+                        }
+
+                        else -> {
+                            log("unexpected button ${buttonNumber} event type ${event.type} in state ${state.pointerEventState}")
+                            state.setPointerEventState(PointerEventState.START)
+                        }
+                    }
+                }
+
+                PointerEventType.Move -> {
+                    when (state.pointerEventState) {
+                        PointerEventState.BUTTON_BOX_PRESS -> {
+                            state.setPointerEventState(PointerEventState.BUTTON_MOVE)
+                        }
+
+                        PointerEventState.BUTTON_MOVE -> {
+                        }
+
+                        else -> {
+                            log("unexpected button ${buttonNumber} event type ${event.type} in state ${state.pointerEventState}")
+                            state.setPointerEventState(PointerEventState.START)
+                        }
+                    }
+                }
+
+                PointerEventType.Release -> {
+                    when (state.pointerEventState) {
+                        PointerEventState.BUTTON_BOX_PRESS -> {
+                            state.setPointerEventState(PointerEventState.BUTTON_RELEASE)
+                        }
+
+                        PointerEventState.BUTTON_MOVE -> {
+                            state.setPointerEventState(PointerEventState.BUTTON_RELEASE)
+                        }
+
+                        else -> {
+                            log("unexpected button ${buttonNumber} event type ${event.type} in state ${state.pointerEventState}")
+                            state.setPointerEventState(PointerEventState.START)
+                        }
+                    }
+                }
+
+                else ->
+                    log("unexpected button ${buttonNumber} event type ${event.type}")
+            }
+            if (state.dirty)
+                stateChanged(state)
+        }
+}
